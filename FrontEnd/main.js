4da40818da4500951274d83b448b5e38ca847373
Matthieu Bazeugeaud <mbazeugeauddevweb@gmail.com>
2025-04-17 13:28:14 +0200

    début d'ajout galerie en JS

--- FrontEnd/main.js
+++ FrontEnd/main.js
@@ -1,10 +1,19 @@
-// Appel à l'API pour récupérer les projets 
+// Appel à l'API pour récupérer les projets depuis le serveur
 fetch("http://localhost:5678/api/works")
-    // Puis on transforme la réponse en JSON
-    .then(reponse => reponse.json())
-    // Puis on va traiter les données reçues
-    .then(data => {
-        // On vérifie la réponse dans la console pour afficher les données
-        console.log(data);
+  // Puis on transforme la réponse en JSON
+  .then((reponse) => reponse.json())
+  // Puis on va traiter les données reçues
+  .then((data) => {
+    // On vérifie la réponse dans la console pour afficher les données, puis après on les affichera dans le DOM
+    console.log(data);
+  });
 
-    })
+const gallery = document.querySelector(".gallery");
+
+data.forEach((project) => {
+  const figure = document.createElement("figure");
+  const img = document.createElement("img");
+  const figcaption = document.createElement("figcaption");
+
+  project.
+});
