9b7c3e4f4c1be5c286d6b5fee81db423771d99f9
Matthieu Bazeugeaud <mbazeugeauddevweb@gmail.com>
2025-04-18 11:12:29 +0200

    creation bouton html et relier au catégorie de l'api avec Antoine

--- FrontEnd/main.js
+++ FrontEnd/main.js
@@ -1,8 +1,9 @@
 // Selection de l'élement HTML qui va contenir les projets
 const gallery = document.querySelector(".gallery");
+const filtre = document.getElementById("filtre");
 
 // Appel de l'API pour récupérer les projets depuis le serveur et les affiche dynamiquement
-async function apiWorks () {fetch("http://localhost:5678/api/works")
+function apiWorks () {fetch("http://localhost:5678/api/works")
 
   // Puis on transforme la réponse en JSON
   .then((reponse) => reponse.json())
@@ -38,37 +39,62 @@ async function apiWorks () {fetch("http://localhost:5678/api/works")
   
 }
 
-apiWorks();
+function apiCategorie() {fetch("http://localhost:5678/api/categories")
+  .then((reponse) => reponse.json())
+  .then((data) => {
+    data.forEach((categorie) => {
+      const btn = document.createElement("button");
+      btn.classList.add("filtre_bouton");
+      btn.textContent = categorie.name;
+      filtre.appendChild(btn);
+    })
+  })
+}
 
-// Une fois la boucle parametrer, on appel la fonction pour lancer l'affichage
+function filtreProjets () {
+  const btnFiltre = document.querySelectorAll(".filtre_bouton");
+  const tab = apiWorks();
+  const retour = [];
+  function filtreTab (categorie) {
+    tab.forEach((projet) => {
+      if (projet.categorie.name === categorie)
+        retour.push (projet);
+    })
+    return retour;
+  }
+}
+
+apiWorks();
+apiCategorie();
+
 /*async function getworks(){
   let works;
   fetch("http://localhost:5678/api/works")
 
-  // Puis on transforme la réponse en JSON
+  
   .then((reponse) => reponse.json())
   .then(  works=reponse)
 
-  // Puis on va traiter les données reçues
+  
   return works;
 }
 async function afficherworks(works){
   gallery.innerHTML = "";
 
-    //Boucle pour chaque projet reçu de l'API
+    
     works.forEach((project) => {
 
-      //Création des elements HTML
+      
       const figure = document.createElement("figure");
       const img = document.createElement("img");
       const figcaption = document.createElement("figcaption");
 
-        // Puis on ajoute les données du projet dans les balises
+        
         img.src = project.imageUrl;
         img.alt = project.title;
         figcaption.textContent = project.title;
     
-        // Pour finir, on construit la structure HTML et on l'insert dans le DOM
+        
         figure.appendChild(img)
         gallery.appendChild(figure)
         figure.appendChild(figcaption)
